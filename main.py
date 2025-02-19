7523d3069fd002034b18c6eb691d432040aef14e
Bensonio <benson.yeung27@gmail.com>
2025-02-19 09:15:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import tkinter as tk
 
-#creates main window
+
 wind = tk.Tk()
-wind.title(Time Tracker App)
-wind.geometry("1920x1080")
+screen_width = wind.winfo_screenwidth()
+screen_height = wind.winfo_screenheight()
+#creates main window
 
+wind.title("Time Tracker App")
+print(screen_width, screen_height)
+wind.geometry(f"{screen_width}x{screen_height}")
+wind.minsize("400x400")
 
-'''
-Widgets Added Here
-'''
+timer = tk.Label(wind, text=f"total time = {}", font=("Arial", 16))
+timer.pack(pady=20)
 
+def update_timer():
+    timer.config(text=f"total time = {}")
+    wind.after(10000,update_timer)
 
 wind.mainloop()
