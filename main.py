cd01a1f0b92d88eae9ff6e576bbb122f32535fe4
Bensonio <benson.yeung27@gmail.com>
2025-02-19 08:21:09 -0500

    Added 1 comment Time tracking feature.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import tkinter as tk
 #creates main window
 wind = tk.Tk()
 wind.title(Time Tracker App)
-wind.geometry("1920, 1080")
+wind.geometry("1920x1080")
+
+
+'''
+Widgets Added Here
+'''
+
 
 wind.mainloop()
