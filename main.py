861fcde6b6c4f2a05531a2171366d076d305ece2
beigeowl <ellison.d.lu@gmail.com>
2025-02-25 08:28:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ screen_height = wind.winfo_screenheight()
 wind.title("Time Tracker App")
 print(screen_width, screen_height)
 wind.geometry(f"{screen_width}x{screen_height}")
-wind.minsize("400x400")
+#wind.minsize("400x400")
 
-timer = tk.Label(wind, text=f"total time = {}", font=("Arial", 16))
+timer = tk.Label(wind, text=f"total time = {0}", font=("Arial", 16))
 timer.pack(pady=20)
 
 def update_timer():
-    timer.config(text=f"total time = {}")
-    wind.after(10000,update_timer)
+    timer.config(text=f"total time = {10}")
+wind.after(10000,update_timer)
 
 wind.mainloop()
