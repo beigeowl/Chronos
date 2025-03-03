ff25c53546ffc010d09964d9567b6e840a9f2f42
Bensonio <benson.yeung27@gmail.com>
2025-03-03 09:20:15 -0500

    Started Class Based App in Main.py, started widget class (menu)

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import tkinter as tk
+from tkinter import ttk
+
+class createApp(tk.Tk): 
+     #creates an instance of the main window as a class, creates the main application window
+     #Essentially the same thing as root = tk.Tk()
+
+    def __init__(self):    
+        super().__init__() #ensures tk.Tk works properly in a class
+        
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        
+
+        #Main Setup
+        self.title('Chronos')
+        self.geometry(f'{screen_width}x{screen_height}') #Fills the window
+        self.minsize(screen_width,screen_height) #Ensures window does not shrink
+
+        #widgets
+        self.menu = Menu(self)
+
+        self.mainloop()
+
+class Menu(ttk.Frame):
+    def __init__(self, parent): 
+        #Parent used to pass the newfound widget/object through the master/App
+        #Creates Frame
+        super().__init__(parent)
+        ttk.Label(self)#.pack(expand = True, fill = 'both') # Creates Widget, comment fills the window
 
 
-wind = tk.Tk()
-screen_width = wind.winfo_screenwidth()
-screen_height = wind.winfo_screenheight()
-#creates main window
-
-wind.title("Time Tracker App")
-print(screen_width, screen_height)
-wind.geometry(f"{screen_width}x{screen_height}")
-#wind.minsize("400x400")
-
-timer = tk.Label(wind, text=f"total time = {0}", font=("Arial", 16))
-timer.pack(pady=20)
-
-def update_timer():
-    timer.config(text=f"total time = {10}")
-wind.after(10000,update_timer)
-
-wind.mainloop()
+createApp()
